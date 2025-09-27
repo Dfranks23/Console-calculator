b82e3d978b7a21b537647decf9731ed20ed86060
Dustin Franks <890006115@ciat.edu>
2025-09-27 16:13:06 -0400

    added pow feature

--- console calculator.py
+++ console calculator.py
@@ -15,11 +15,12 @@ print("1.add")
 print("2.subtract")
 print("3.multiply")
 print("4.divide")
+print("5.exponent")
 
 while True:
-    choice = input("select 1/2/3/4 = ")
+    choice = input("select 1/2/3/4/5 = ")
 
-    if choice in ('1', '2', '3', '4'):
+    if choice in ('1', '2', '3', '4','5'):
         try:
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
@@ -39,9 +40,12 @@ while True:
     elif choice == '4':
         print(num1, "/", num2, "=", divide(num1, num2))
 
+    elif choice == '5':
+        print(num1, "exponent", num2, "=", pow(num1, num2))
 
     next_calculation = input("Let's do next calculation? (yes/no): ")
     if  next_calculation == "no":
+        print("Exiting calculator")
         break
     else:
-        print("Invalid Input")
+        print("next calculation")
